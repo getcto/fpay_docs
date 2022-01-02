f0bfb764d3dfd7168b2c26fdc5618ad8c2f67c0c
kengkeelim <limkengkee88@gmail.com>
2022-01-02 13:57:09 +0800

    Update content

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -51,7 +51,7 @@ const config = {
         items: [
           {
             type: 'doc',
-            docId: 'intro',
+            docId: 'introduction',
             position: 'left',
             label: 'Documentation',
           },
